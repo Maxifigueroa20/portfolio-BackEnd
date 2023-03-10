4dac9846401188d12a749c247cfad0b7749523ec
Maximiliano <marianof0101@gmail.com>
2023-03-10 01:57:23 -0300

    initial commit

---
+++ src/main/java/com/portfolio/maximilianofigueroa/MaximilianofigueroaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.maximilianofigueroa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximilianofigueroaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaximilianofigueroaApplication.class, args);
+	}
+
+}
