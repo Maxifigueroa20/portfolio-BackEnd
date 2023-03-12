b534c880f6534a899312ccb601c87000a14d5fa9
Maximiliano <marianof0101@gmail.com>
2023-03-12 03:50:34 -0300

    Configuracion de la interfaz ProyectoRepository

--- src/main/java/com/portfolio/maximilianofigueroa/repository/ProyectoRepository.java
+++ src/main/java/com/portfolio/maximilianofigueroa/repository/ProyectoRepository.java
@@ -1,4 +1,9 @@
 package com.portfolio.maximilianofigueroa.repository;
 
-public interface ProyectoRepository {
+import com.portfolio.maximilianofigueroa.model.Proyecto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProyectoRepository extends JpaRepository <Proyecto, Long> {
 }
