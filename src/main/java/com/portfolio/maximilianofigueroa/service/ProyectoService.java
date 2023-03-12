a5d49cfe3b295b27db270f572fa0e76cc030f29d
Maximiliano <marianof0101@gmail.com>
2023-03-12 03:57:31 -0300

    Configuracion de la clase ProyectoService

--- src/main/java/com/portfolio/maximilianofigueroa/service/ProyectoService.java
+++ src/main/java/com/portfolio/maximilianofigueroa/service/ProyectoService.java
@@ -1,4 +1,41 @@
 package com.portfolio.maximilianofigueroa.service;
 
+
+import com.portfolio.maximilianofigueroa.model.Proyecto;
+import com.portfolio.maximilianofigueroa.repository.ProyectoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class ProyectoService {
+    private final ProyectoRepository proyectoRepository;
+
+    @Autowired
+    public ProyectoService(ProyectoRepository proyectoRepository) {
+        this.proyectoRepository = proyectoRepository;
+    }
+
+    public Proyecto agregarProyecto(Proyecto proyecto) {
+        return proyectoRepository.save(proyecto);
+    }
+
+    public Proyecto editarProyecto(Proyecto proyecto) {
+        return proyectoRepository.save(proyecto);
+    }
+
+    public List<Proyecto> verProyectos() {
+        return proyectoRepository.findAll();
+    }
+
+    public Proyecto buscarProyecto(Long id) {
+        return proyectoRepository.findById(id).orElse(null);
+    }
+
+    public void borrarProyecto(Long id) {
+        proyectoRepository.deleteById(id);
+    }
 }
