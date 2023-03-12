e96c0326663980ae7c8b389962c10546d6d4ef22
Maximiliano <marianof0101@gmail.com>
2023-03-12 03:45:06 -0300

    Configuracion de la clase Proyecto

--- src/main/java/com/portfolio/maximilianofigueroa/model/Proyecto.java
+++ src/main/java/com/portfolio/maximilianofigueroa/model/Proyecto.java
@@ -1,4 +1,34 @@
 package com.portfolio.maximilianofigueroa.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter @Setter
+@Entity
 public class Proyecto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idProyecto;
+    private String tituloProyecto;
+    private String fechaProyecto;
+    private String descripcionProyecto;
+    private String imagenProyecto;
+    private String urlProyecto;
+
+    public Proyecto() {}
+
+    public Proyecto(Long idProyecto, String tituloProyecto, String fechaProyecto, String descripcionProyecto, String imagenProyecto, String urlProyecto) {
+        this.idProyecto = idProyecto;
+        this.tituloProyecto = tituloProyecto;
+        this.fechaProyecto = fechaProyecto;
+        this.descripcionProyecto = descripcionProyecto;
+        this.imagenProyecto = imagenProyecto;
+        this.urlProyecto = urlProyecto;
+    }
 }
